72869a1ebb9aeb316ab9a2a8f5c599b5ef12cdf0
VKoorits <koorits.viktor@yandex.ru>
2016-10-03 00:40:32 +0300

    corrected error crossing

--- src/ray2d.hpp
+++ src/ray2d.hpp
@@ -7,152 +7,151 @@ enum Direction{UP, DOWN};
 
 class Ray2D
 {
-public:
-  // Allow default constructor.
-  Ray2D() = default;
+  public:
+    Ray2D()=default;
+    Ray2D(Point2D& origin,  float direction)
+      :m_origin(origin),
+       m_direction(direction-int(direction/360.0f)*360.0f )//аналогично direction%360
+      {
+        if(direction<0.0f)
+          direction+=360.0f;
+      }
+    Ray2D(float originX, float originY,  float direction)
+      :m_origin(originX, originY),
+       m_direction(direction-int(direction/360.0f)*360.0f )//аналогично direction%360
+      {
+        if(direction<0.0f)
+          direction+=360.0f;
+      }
+    Ray2D(std::initializer_list<float> const & lst)
+    {
+      auto it=lst.begin();
+      m_origin.x(*it);
+      it++;
 
-  // Constructor with parameters.
-  Ray2D(Point2D & origin, float direction)
-    :m_origin(origin),
-     m_direction(direction-int(direction/360.0f)*360.0f )// direction%360
-    {
-      if(direction<0.0f)
-        direction+=360.0f;
-    }
-  Ray2D(float originX, float originY, float direction)
-    :m_origin(originX, originY),
-     m_direction(direction-int(direction/360.0f)*360.0f )// direction%360
-    {
-      if(direction<0.0f)
-        direction+=360.0f;
+      if(it!=lst.end())
+      {
+          m_origin.y(*it);
+          it++;
+      }
+
+      if(it!=lst.end())
+      {
+        m_direction= *it-int(*it/360.0f)*360.0f;
+        if(m_direction<0.0f)
+          m_direction+=360.0f;
+      }
     }
 
-  // Constructor with initialization list.
-  Ray2D(std::initializer_list<float> const & lst)
-  {
-    auto it=lst.begin();
-    m_origin.x()=*it;
-    it++;
-    if(it!=lst.end())
+    float const X(){return m_origin.x();}
+    float const Y(){return m_origin.y();}
+    float const direction(){return m_direction;}
+    Point2D const origin(){return m_origin;}
+    void X(float x){m_origin.x(x);}
+    void Y(float y){m_origin.y(y);}
+    void direction(float direction)
     {
-        m_origin.y()=*it;
-        it++;
-    }
-    if(it!=lst.end())
-    {
-      m_direction= *it-int(*it/360.0f)*360.0f;
+      m_direction = direction-int(direction/360.0f)*360.0f;
       if(m_direction<0.0f)
         m_direction+=360.0f;
     }
-  }
+    void origin(Point2D& origin){m_origin=origin;}
 
-  // Copy constructor
-  Ray2D(Ray2D const & obj)
-    : m_origin(obj.m_origin), m_direction(obj.m_direction)
-  {}
+    bool operator ==(Ray2D const & Ray) const
+    {
+      return(m_origin==Ray.m_origin && fabs(m_direction-Ray.m_direction)<kEps);
+    }
+    bool operator !=(Ray2D const & Ray) const
+    {
+      return !(m_origin==Ray.m_origin && fabs(m_direction-Ray.m_direction)<kEps);
+    }
+    bool operator &&(Box2D& Box)
+    {
+      float k=tan(m_direction*RAD);
+      float b=m_origin.y()-k*m_origin.x();
+      float coord=0.0f;
 
-  // Getters
-  inline Point2D & origin() { return m_origin; }
-  inline float const x(){return m_origin.x();}
-  inline float const y(){return m_origin.y();}
-  inline float & direction() { return m_direction; }
-  inline Point2D const & origin() const { return m_origin; }
-  inline float const & direction() const { return m_direction; }
+      //right
+      coord=k*Box.right()+b;
+      if(coord<=Box.top()+kEps && coord>=Box.bottom()-kEps && checkPoint(Box.right(), coord))
+        return true;
+      //left
+      coord=k*Box.left()+b;
+      if(coord<=Box.top()+kEps && coord>=Box.bottom()-kEps && checkPoint(Box.left(), coord))
+        return true;
+      //top
+      coord=float((Box.top()-b))/k;
+      if(fabs(coord)<kEps)
+          coord=0;
+      if(coord<=Box.right()+kEps && coord>=Box.left()-kEps && checkPoint(coord, Box.top()))
+        return true;
 
-  // Setters
-  void direction(float direction)
-  {
-    m_direction = direction-int(direction/360.0f)*360.0f;
-    if(m_direction<0.0f)
-      m_direction+=360.0f;
-  }
-  void origin(Point2D& origin){m_origin=origin;}
+      return false;
+    }
+    bool operator &&(Point2D& point)
+    {
+      float k=tan(m_direction*RAD);
+      float b=m_origin.y()-k*m_origin.x();
 
-  // Assignment operator.
-  Ray2D & operator = (Ray2D const & obj)
-  {
-    if (this == &obj) return *this;
-    m_origin = obj.m_origin;
-    m_direction = obj.m_direction;
-    return *this;
-  }
+      float coord=k*point.x()+b;
 
-  // Logical operators
-  bool operator == (Ray2D const & obj) const
-  {
-    return (m_origin == obj.m_origin && m_direction == obj.m_direction);
-  }
+      if(abs(coord-point.y())<kEps)
+        return true;
+      return false;
+    }
 
-  bool operator != (Ray2D const & obj) const
-  {
-    return !operator==(obj);
-  }
+    float operator [] (unsigned int i) const
+    {
+      switch(i)
+      {
+        case 0:
+          return m_origin.x();
+          break;
+        case 1:
+          return m_origin.y();
+          break;
+        case 2:
+          return m_direction;
+          break;
+        default:
+          return 0.0f;
+      }
+    }
 
-  // Capabilities
-  bool operator &&(Box2D& Box)
-  {
-    float k=tan(m_direction*RAD);
-    float b=m_origin.y()-k*m_origin.x();
-    float coord=0.0f;
-    Direction orient=UP;//1==ray going to the left, 0-to the right from point
-    if(m_direction>=180.0f)
-      orient=DOWN;
-    //right
-    coord=k*Box.right()+b;
-    if( ( (orient==UP && Box.top()+kEps>=m_origin.y()) || (orient==DOWN && Box.bottom()-kEps<=m_origin.y()))//отсекаем ненужную половину луча
-        && coord<=Box.top()+kEps && coord>=Box.bottom()-kEps)
-      return true;
-    //left
-    coord=k*Box.left()+b;
-    if( ( (orient==UP && Box.top()+kEps>=m_origin.y()) || (orient==DOWN && Box.bottom()-kEps<=m_origin.y()))
-        && coord<=Box.top()+kEps && coord>=Box.bottom()-kEps)
-      return true;
-    //top
-    coord=float((Box.top()-b))/k;
-    if( ( (orient==UP && Box.top()+kEps>=m_origin.y()) || (orient==DOWN && Box.bottom()-kEps<=m_origin.y()))
-        && coord<=Box.right()+kEps && coord>=Box.left()-kEps)
-      return true;
-    return false;
-  }
+    void VerticalShift(float shift){m_origin.VerticalShift(shift);}
+    void HorizontalShift(float shift){m_origin.HorizontalShift(shift);}
+    void DirectionShift(float shift)
+    {
+      m_direction+=shift;
+      m_direction = m_direction-int(m_direction/360.0f)*360.0f;
+      if(m_direction<0.0f)
+        m_direction+=360.0f;
+    }
 
-  bool operator &&(Point2D& point)
-  {
-    float k=tan(m_direction*RAD);
-    float b=m_origin.y()-k*m_origin.x();
+    friend std::ostream& operator << (std::ostream & os, Ray2D const & obj)
+    {
+      os << "Ray2D {" << obj.m_origin << ", " << obj.m_direction << "}";
+      return os;
+    }
+  private:
+    bool checkPoint(float x, float y)
+    {
+        if(m_direction<=180.0f && y<m_origin.y())
+            return false;
 
-    float coord=k*point.x()+b;
+        if(m_direction>180 && y>=m_origin.y())
+            return false;
 
-    if(fabs(coord-point.y())<kEps)
-      return true;
-    return false;
-  }
+        if((m_direction<=90 || m_direction>=270) && x<m_origin.x())
+            return false;
 
-  void VerticalShift(float shift){m_origin.VerticalShift(shift);}
-  void HorizontalShift(float shift){m_origin.HorizontalShift(shift);}
-  void DirectionShift(float shift)
-  {
-    m_direction+=shift;
-    m_direction = m_direction-int(m_direction/360.0f)*360.0f;
-    if(m_direction<0.0f)
-      m_direction+=360.0f;
-  }
+        if(m_direction>90 && m_direction<270 && x>=m_origin.x())
+            return false;
 
-  // Redefinition
-  float operator [] (unsigned int index) const
-  {
-    if (index >= 3) return 0.0f;
-    return index == 0 ? m_origin.x() : index == 1 ? m_origin.y() : m_direction;
-  }
+        return true;
+    }
+    Point2D m_origin={0.0f, 0.0f};
+    float m_direction=0.0f;
 
-  friend std::ostream & operator << (std::ostream & os, Ray2D const & obj)
-  {
-    os << "Ray2D {" << obj.origin() << ", " << obj.direction() << "}";
-    return os;
-  }
-
-private:
-
-  Point2D m_origin = {0.0f, 0.0f};
-  float m_direction = 90.0f; //UP
 };
 
